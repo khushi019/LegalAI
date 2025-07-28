5c3323b8a68d308133f78733d2edced9387735b2
khushi019 <khushikhandelwal419@gmail.com>
2025-07-28 23:11:24 +0530

    admin commit

--- document_analyzer/admin.py
+++ document_analyzer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Document, Clause
 
-# Register your models here.
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uploaded_at', 'user', 'processed')
+    list_filter = ('processed', 'uploaded_at')
+    search_fields = ('title', 'user__username')
+
+@admin.register(Clause)
+class ClauseAdmin(admin.ModelAdmin):
+    list_display = ('document', 'page_number', 'position')
+    list_filter = ('page_number',)
+    search_fields = ('text', 'document__title')
